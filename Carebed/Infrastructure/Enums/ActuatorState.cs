8a6996d331bbb2ddc922f20e67e91be631707745
Matt Schatz <Macrog@thexyz.com>
2025-11-16 14:45:51 -0500

    Added ActuatorState enum - hold valid states that an actuator can be in

---
+++ Carebed/Infrastructure/Enums/ActuatorState.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Carebed.Infrastructure.Enums
+{
+    public enum ActuatorState
+    {
+        Idle,           // The actuator is not currently moving or engaged
+        Moving,         // The actuator is in the process of moving
+        Completed,      // The actuator has completed its movement
+        Error,          // The actuator has encountered an error condition
+        Locked,         // The actuator is locked and cannot move
+        Initializing    // The actuator is in an initializing state and will be ready soon
+    }
+}
